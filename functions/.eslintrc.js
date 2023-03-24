14889333455d76ed269567df24dfffe00ffa9884
Poojesh Shetty <poojesh@wednesday.is>
2023-03-24 17:15:17 +0530

    feat: update functions configuration

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,32 +1,30 @@
+const prettierOptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'));
+
 module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true,
+    node: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
+    // project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: 'module'
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
+    '/lib/**/*' // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2]
-  },
+    'import/no-unresolved': 0,
+    'import/no-webpack-loader-syntax': 0
+  }
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,8 +2,10 @@ import * as functions from "firebase-functions";
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+export const helloWorld = functions.https.onRequest(
+  (request: any, response: { send: (arg0: string) => void }) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+  }
+);
