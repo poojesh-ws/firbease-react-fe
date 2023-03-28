080cb0e14091b2149fa629ef5ccb01094c6a1637
Poojesh Shetty <poojesh@wednesday.is>
2023-03-28 17:52:08 +0530

    feat: add github actions for functions

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,9 +1,7 @@
-const prettierOptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'));
-
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
   },
   extends: [
     'eslint:recommended',
@@ -11,19 +9,19 @@ module.exports = {
     'plugin:import/warnings',
     'plugin:import/typescript',
     'google',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     // project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: 'module'
+    sourceType: 'module',
   },
   ignorePatterns: [
-    '/lib/**/*' // Ignore built files.
+    '/lib/**/*', // Ignore built files.
   ],
   plugins: ['@typescript-eslint', 'import'],
   rules: {
     'import/no-unresolved': 0,
-    'import/no-webpack-loader-syntax': 0
-  }
+    'import/no-webpack-loader-syntax': 0,
+  },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest(
-  (request: any, response: { send: (arg0: string) => void }) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-  }
+    (_, response: { send: (arg0: string) => void }) => {
+      functions.logger.info('Hello logs!', {structuredData: true});
+      response.send('Hello from Firebase!');
+    }
 );
